2c50df86132af669ae09f741bdcb167d119c84d3
skatedplrn <evgeny@dplrn.com>
2017-12-15 12:05:44 +0300

    refactor messenger

--- tpot/auger_messenger.py
+++ tpot/auger_messenger.py
@@ -8,31 +8,30 @@ from .pipeline_exports import collect_feature_list, serialize_to_js
 class AugerMessenger:
     def __init__(self, conn_info):
         self.conn_info = conn_info
+        self.uid = uuid.uuid4().hex[:15].upper()
+
         if self.conn_info:
-            self.uid = uuid.uuid4().hex[:15].upper()
             self.r = redis.StrictRedis(host=conn_info['host'], port=conn_info['port'], db=conn_info['db'])
 
     def send_started(self, cv_num):
-        if self.conn_info:
-            json = {'started': 1}
-            self.r.publish(self.conn_info['channel'], pickle.dumps(json))
-            self.r.hset(self.conn_info['channel'], self.uid + '-fold', cv_num)
+        self._send_message({'started': 1})
+        #self.r.hset(self.conn_info['channel'], self.uid + '-fold', cv_num)
 
     def send_scores(self, sklearn_pipeline, features, target, scores):
-        if self.conn_info:
-            sklearn_pipeline_json = self._format_pipeline_json(sklearn_pipeline.steps,features,target)
-            sklearn_pipeline_json['score'] = np.nanmean(scores)
-            json = {self.uid: sklearn_pipeline_json}
-            self.r.publish(self.conn_info['channel'], pickle.dumps(json))
+        sklearn_pipeline_json = self._format_pipeline_json(sklearn_pipeline.steps,features,target)
+        sklearn_pipeline_json['score'] = np.nanmean(scores)
+        self._send_message({self.uid: sklearn_pipeline_json})
 
     def send_total_eval(self, total_evals):
-        if self.conn_info:
-            total_eval_str = pickle.dumps({'total_evaluation': total_evals})
-            self.r.publish(self.conn_info['channel'], total_eval_str)
+        self._send_message({'total_evaluation': total_evals})
 
     def send_status_eval(self, status):
+        self._send_message({'evaluation_status': status})
+
+    def _send_message(self, msg):
+        print(msg)
         if self.conn_info:
-            self.r.publish(self.conn_info['channel'], pickle.dumps({'evaluation_status': status}))
+            self.r.publish(self.conn_info['channel'], pickle.dumps(msg))
 
     def _format_pipeline_json(self, pipeline,features,target):
         json = {'pipeline_list':[],'func_dict':{}}
